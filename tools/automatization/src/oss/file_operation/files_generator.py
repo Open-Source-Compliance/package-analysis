9c199a40f63e57647ef0e5b03e77c5c409a17115
Parian Golchin <parian.golchin@iris-sensing.com>
2024-05-27 17:10:51 +0200

    Apply some fixes
    1. Generate document also for windows os with empty space inside folder path
    2. Update readme
    3. Update the name of the file in ossdisclosure (only of you find the library name and also library version)
    4. Make validation configurable

--- tools/automatization/src/oss/file_operation/files_generator.py
+++ tools/automatization/src/oss/file_operation/files_generator.py
@@ -1,8 +1,12 @@
 import os
+from oss.utils.command_line_parser import parser
 from oss.content_analyzer.read_regex_replacement import ReadRegexReplacement
 from oss.types.operation_case import OperationCase
 from oss.folder_operation.folder_reader import FolderReader
 from oss.file_operation.file_name_parser import FileNameParser
+from oss.types.operation_boolean import OperationBoolean
+
+parser.add_argument('-v', '--validation',  type=OperationBoolean, choices=list(OperationBoolean), help='Validate all the generated files(default value is %(default)s)' , default= "true")
 
 
 class FilesGeneratorException(Exception):
@@ -37,16 +41,17 @@ class FilesGenerator:
         self.final_file_path = ""
 
     def validate_file(self, input_file):
-        command = f"pyspdxtools -i {input_file}"
+        command = f"pyspdxtools -i \"{input_file}\""
         os.system(command)
         print(">>>>> Validate Document name: ",  input_file)
 
     def generate_file(self, input_file):
         generated_file_path = os.path.join(self.generated_folder_path, input_file)
-        command = f"pyspdxtools -i {self.final_file_path} -o {generated_file_path}"
+        command = f"pyspdxtools -i \"{self.final_file_path}\" -o \"{generated_file_path}\""
         os.system(command)
         print(">>>>> Generated Document name:",  generated_file_path)
-        return generated_file_path
+        if parser["validation"] == OperationBoolean.true:
+            self.validate_file(generated_file_path)
 
     def generate_files(self):
         result_txt = self.generate_txt_files()
@@ -61,18 +66,15 @@ class FilesGenerator:
     
     def spdx_to_json(self, packet_name):
         generated_spdx_json = packet_name + ".spdx.json"
-        generated_file_path = self.generate_file(generated_spdx_json)
-        self.validate_file(generated_file_path)
+        self.generate_file(generated_spdx_json)
 
     def spdx_to_yaml(self, packet_name):
         generated_spdx_yaml = packet_name + ".spdx.yaml"
-        generated_file_path = self.generate_file(generated_spdx_yaml)
-        self.validate_file(generated_file_path)
+        self.generate_file(generated_spdx_yaml)
 
     def spdx_to_rdf_xml(self, packet_name):
         generated_spdx_rdf_xml = packet_name + ".spdx.rdf.xml"
-        generated_file_path = self.generate_file(generated_spdx_rdf_xml)
-        self.validate_file(generated_file_path)
+        self.generate_file(generated_spdx_rdf_xml)
 
     def save_new_file(self, final_file, file_name):
         if self.generated_folder_path:
